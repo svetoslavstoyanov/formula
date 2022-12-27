c48733f43fb13e54f9589213e9c0788b53b2e3bf
Svetoslav Stoyanov <svetoslav39@gmail.com>
2022-12-28 01:05:30 +0200

    Added comments

--- src/main/java/com/formula/one/FormulaOneApplication.java
+++ src/main/java/com/formula/one/FormulaOneApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FormulaOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormulaOneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FormulaOneApplication.class, args);
+    }
+// TODO: api for updating driver race info
+// TODO: raceRanking service
+// TODO: race service
+// TODO: race controller
+// TODO: after each race update drivers race info
+// TODO: query/mutations for generating data
 
+// TODO*: add status for race? READY, RUNNING, FINISHED
+// TODO*: add years
+// TODO*: add stints for race info for drivers per race
 }
